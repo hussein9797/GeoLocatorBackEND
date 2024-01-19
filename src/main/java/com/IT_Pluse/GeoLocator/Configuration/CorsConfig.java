7f6c9bf2f1e6f766630775982beaca869c564f7d
Hussein <husseinyousf505@gmail.com>
2024-01-19 17:03:50 +0300

    FIRST COMMIT

--- src/main/java/com/IT_Pluse/GeoLocator/Configuration/CorsConfig.java
+++ src/main/java/com/IT_Pluse/GeoLocator/Configuration/CorsConfig.java
@@ -2,7 +2,7 @@ package com.IT_Pluse.GeoLocator.Configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
--- src/main/java/com/IT_Pluse/GeoLocator/Configuration/MailConfigurations.java
+++ src/main/java/com/IT_Pluse/GeoLocator/Configuration/MailConfigurations.java
@@ -1,10 +1,8 @@
 package com.IT_Pluse.GeoLocator.Configuration;
 
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
+
 
 @Configuration
 public class MailConfigurations {
