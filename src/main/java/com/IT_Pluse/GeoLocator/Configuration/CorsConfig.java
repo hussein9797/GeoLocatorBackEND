f4d3de5d1699f4a101146e392cf67ba596c9db5d
Hussein <husseinyousf505@gmail.com>
2024-01-19 17:10:17 +0300

    add git ignor

--- src/main/java/com/IT_Pluse/GeoLocator/Configuration/CorsConfig.java
+++ src/main/java/com/IT_Pluse/GeoLocator/Configuration/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
 
                 .allowedOriginPatterns("http://localhost:8888/")
